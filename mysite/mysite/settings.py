0408ef569753d08bd06aa2b6c5c277d33a7f6ae4
Ciprian Constantin <ciprianfc@pop-os.localdomain>
2022-11-04 19:22:34 +0200

    Add route app and some changes to mysite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rsrb-3pw4*6o%kqv)s)yf%%=j$h#^gie(s0@anvps*!5jacm=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #sample applications
+    'route.apps.RouteConfig',
 ]
 
 MIDDLEWARE = [
