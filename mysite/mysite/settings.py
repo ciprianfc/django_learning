c783a78e396744e9ad1d80c7f80d3f4d05d1a99d
Ciprian Constantin <ciprianfc@pop-os.localdomain>
2022-11-09 11:32:02 +0200

    fix title

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Used for a default title
+APP_NAME = 'Django learning'   # Add
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -75,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'home.context_processors.settings',
             ],
         },
     },
